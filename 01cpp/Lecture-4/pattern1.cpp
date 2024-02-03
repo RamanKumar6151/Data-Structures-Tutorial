0f9c0ba3ade79831eb9aba35ee9b2df295b78968
Raman <ramankp655@gmail.com>
2024-02-03 14:27:15 +0530

    3 feb 2024 commit 2, added python files

---
+++ 01cpp/Lecture-4/pattern1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+    system("CLS");
+    int r, c;
+    cout<<"enter the amount of rows and columns"<<endl;
+    cin>>r>>c;
+    int i=0, j=0;
+    while(i<r){
+        while(j<c){
+            cout<<i+1;
+            j++;
+        }
+        cout<<endl;
+        j=0;
+        i++;
+    }
+}

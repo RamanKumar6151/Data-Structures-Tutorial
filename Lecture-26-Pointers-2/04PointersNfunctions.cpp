4d277152728bfaa89365c3580f45e008f35b89f6
RamanKumar656 <ramankp656@gmail.com>
2026-01-28 10:11:02 +0530

    Completed lecture 26

--- Lecture-26-Pointers-2/04PointersNfunctions.cpp
+++ Lecture-26-Pointers-2/04PointersNfunctions.cpp
@@ -8,9 +8,24 @@ void print(int *p){
 }
 
 // fucntion to update the pointer
-void update(int *p){
+int update(int *p){
     p=p+1;
     // *p=*p+1;
+    return *p;
+}
+
+// function to sum all the elements of an array
+// int getSum(int arr[], int n){
+int getSum(int *arr, int n){
+// int getSum(int arr, int n){  // here you passed the arr as an integer instead of pointer which is mandatory to wrok with array
+    int sum=0;
+    cout<<sizeof(arr)<<endl;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        // sum+=*(arr+i); // trying to configure with int getSum(int arr, int n), but will give an array because here you are using the pointer object
+        // you cannot work with pointer in a function without passing the same pointer in function parameters
+    }
+    return sum;
 }
 
 int main(){
@@ -27,9 +42,14 @@ int main(){
     // before calling update function
     cout<<"before calling update function "<<&p<<endl;
     // calling update function
-    update(p);
+    int q=update(p);
+    cout<<q<<endl;
     // after calling update function
     cout<<"after calling update function "<<&p<<endl;
+
+    int arr[5]={1,2,3,4,5};
+    int sum=getSum(arr, 5);
+    cout<<"sum of all elements in arr array is "<<sum<<endl;
 
     return 0;
 }
